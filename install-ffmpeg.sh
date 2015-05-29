46ad3e13e1af21663dc5c36b05ad3224ade75f41
mariogasparoni <mariogasparoni@gmail.com>
2015-05-29 10:26:10 -0300

    Updates for custom 2.6.3-090sipvideo version
    Updated install-ffmpeg.sh script

--- install-ffmpeg.sh
+++ install-ffmpeg.sh
@@ -1,3 +1,3 @@
 sudo ./configure --enable-x11grab --enable-gpl --enable-version3 --enable-postproc --enable-libvorbis --enable-libvpx --enable-librtmp --enable-libx264 --enable-libtheora --enable-libvpx --enable-libwebp --enable-lsp --enable-libmp3lame --enable-libopus --enable-libspeex --enable-avresample --enable-ffplay
 sudo make
-sudo checkinstall --pkgname=ffmpeg --pkgversion="`./version.sh`" --backup=no --deldoc=yes --default
+sudo checkinstall --pkgname=ffmpeg --pkgversion="`cat RELEASE`" --backup=no --deldoc=yes --default
